46fc7ccd1bc6a0d32f755992c4a2abff2bbd3eef
birdbird925 <xiangwen94@gmail.com>
2017-11-01 19:19:19 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,6 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        dd($request->ip());
         $slider = Slider::all();
         $featuredProduct = FeaturedProduct::all();
         return view('home', compact('slider', 'featuredProduct'));
